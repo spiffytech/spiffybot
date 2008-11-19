8901cf5dfdc6be76c4f5f52494f903f665ea56ce
Brian Cottingham <brian@brian-lappy.localdomain>
2008-11-19 17:31:56 -0500

    IP retrieval works

--- client.py
+++ client.py
@@ -1,38 +1,20 @@
+#This program is licensed under the GNU GPLv3
+#Copyright Brian Cottingham <spiffytech@gmail.com>
+#Version 0.1- Basically funtional
+
+import re
 import os
 import socket
 
 #Connection vars
 botNick = "spiffybot"
 hostName = "My Machine"
-serverName= "BotLand"
-realName = "Mr. Bot Dude"
+serverName= "BotLand" #Nickname for origin of connection (your house's network)
+realName = "Mr. Bot Dude" #Shows up under whois
 #channelList = ["#bottest", "#ncsulug", "#nilbus"]
-channelList = ["#bottest"]
-network = 'irc.freenode.net'
-port = 6667
-
-
-def getNick(message):
-    return message.split ( '!' ) [ 0 ].replace ( ':', '' ) 
-
-
-
-def getDest(message):
-    return ''.join ( message.split ( ':' ) [ :2 ] ).split ( ' ' ) [ -2 ]
-
-
-
-def connect(): #Connect to server
-    global irc
-    irc = socket.socket ( socket.AF_INET, socket.SOCK_STREAM )
-    irc.connect ( ( network, port ) )
-    irc.recv ( 4096 )
-    irc.send ( 'NICK ' + botNick + '\r\n' )
-    irc.send ( 'USER ' + botNick + " " + hostName + " " + serverName + " " + ' :' + realName + " " + '\r\n' )
-    for channel in channelList:
-        irc.send ( 'JOIN ' + channel + '\r\n' )
-    #Send entrance banner
-    irc.send ( "PRIVMSG " + channelList[0] + " :Greetings, mortal.\r\n" )
+channelList = ["#bottest"] #List of channels to connect to at startup
+network = 'irc.freenode.net' #IRC server to connect to
+port = 6667 #Connection port
 
 
 
@@ -40,26 +22,26 @@ def main():
     connect()
     #Message reading loop
     while True:
-        data = irc.recv ( 4096 ) # Get data from server
+        data = irc.recv ( 4096 ) # Get next message from server
 
-        #Occasionally respond to connection check
-        if data.find ( 'PING' ) != -1: 
-            irc.send ( 'PONG ' + data.split() [ 1 ] + '\r\n' )
+        #Occasionally respond to connection check by IRC server
+        if data.find ( 'PING' ) != -1: #receive "ping"
+            irc.send ( 'PONG ' + data.split() [ 1 ] + '\r\n' ) #respond "pong"
 
-        #Real message!
+        #If server sent us a real message:
         elif data.find ( 'PRIVMSG' ) != -1:
             message = ':'.join ( data.split ( ':' ) [ 2: ] )
             message = message[0:-2] #IRC messages end with \r\n
             if message[0] == "!" or message[0:len(botNick)] == botNick: #Command for bot
-                nick = getNick(data)
-                destination = getDest(data)
+                nick = getNick(data) #Get nick of message sender
+                destination = getDest(data) #Get channel message came from
 
                 #Parse command from message
                 if message[0] == "!": #!calc 2*2
                     command = message.partition(" ")[0].partition("!")[2]
                     args = message.partition(" ")[2]
-                elif message[0:len(botNick)] == botNick: #spiffybot, calc 2*2
-                    if message[len(botNick)] == ",":
+                elif message[0:len(botNick)] == botNick: #Message starts with bot name
+                    if message[len(botNick)] == ",": #spiffybot, calc 2*2
                         splitter = ", "
                     elif message[len(botNick)] == "-": #spiffybot- calc 2*2
                         splitter = "- "
@@ -75,9 +57,10 @@ def main():
                 #Run the specified command
                 reply = runCommand(command, args)
 
-                #Send the final reply
-                if not locals().has_key("reply"):
+                if not locals().has_key("reply"): #Just in case...
                     reply = "whatever"
+
+                #Send the final reply
                 irc.send ( 'PRIVMSG ' +  destination + ' :' + nick + ': ' + reply + '\r\n' )
 
 
@@ -86,7 +69,27 @@ def runCommand(command, args):
         reply = "No commands"
 
     elif command == "ip":
-        reply = socket.gethostbyname(socket.gethostname())
+        interface = args
+
+        ifconfig = os.popen("/sbin/ifconfig").read().split("\n")
+
+        found = 0
+        for line in range(0, len(ifconfig)):
+            if ifconfig[line].find(interface) != -1:
+                found = 1
+                break
+
+        if found == 0:
+            reply = "no such interface"
+            print "args =", args
+        else:
+            ip = re.findall("addr:\d+.\d+.\d+.\d+", ifconfig[line+1])
+            print "ip = " + str(ip)
+            if len(ip) != 0:
+                reply = ip[0].partition(":")[2] 
+                print "reply = " + reply
+            else:
+                reply = "No IP on that interface"
         
     elif command == "load":
         uptime = os.popen("uptime").read()
@@ -98,6 +101,34 @@ def runCommand(command, args):
     else:
         reply = "no such command"
     return reply
+
+
+
+#Parse sending nick from a received message
+def getNick(message):
+    return message.split ( '!' ) [ 0 ].replace ( ':', '' ) 
+
+
+
+#Parse sending channel from received message
+def getDest(message):
+    return ''.join ( message.split ( ':' ) [ :2 ] ).split ( ' ' ) [ -2 ]
+
+
+
+#Connect to server
+def connect():
+    global irc
+    irc = socket.socket ( socket.AF_INET, socket.SOCK_STREAM )
+    irc.connect ( ( network, port ) )
+    irc.recv ( 4096 )
+    irc.send ( 'NICK ' + botNick + '\r\n' )
+    irc.send ( 'USER ' + botNick + " " + hostName + " " + serverName + " " + ' :' + realName + " " + '\r\n' )
+    for channel in channelList:
+        irc.send ( 'JOIN ' + channel + '\r\n' )
+    #Send entrance banner
+    irc.send ( "PRIVMSG " + channelList[0] + " :Greetings, mortal.\r\n" )
+
 
 
 try: 
