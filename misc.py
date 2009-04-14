95e40146ed2d092468b2d4cbcbe9d0bafd90337f
Brian Cottingham <brian@spiffytop.spiffynet>
2009-04-14 17:05:31 -0400

    Added a function to add a trivia fact. You can't actually call it yet, though.

--- misc.py
+++ misc.py
@@ -8,3 +8,7 @@ def trivia(connection, event, args):
     sender = event.source().split("!")[0]
     fact = cursor.execute("SELECT * FROM trivia ORDER BY RANDOM() LIMIT 1").fetchone()[0]
     connection.privmsg(event.target(), sender+" : "+fact)
+
+def addTrivia(connection, event, args):
+    sender = event.source().split("!")[0]
+    cursor.execute("insert into trivia values (?, ?)", sender, args)
