6b69b122ef40f2e792018ad382272662f3db8f35
Brian Cottingham <brian@spiffytop.spiffynet>
2009-04-14 16:41:22 -0400

    Added a trivia table with loads of quotes to the database

---
+++ tools/importTrivia.py
@@ -0,0 +1,15 @@
+from sqlite3 import dbapi2 as sqlite
+
+dbConn = sqlite.connect("logs.db")
+cursor = dbConn.cursor()
+
+inFile = open("trivia.txt", 'r')
+facts = inFile.readlines()
+inFile.close()
+
+for fact in facts:
+    if fact == "\n":
+        continue
+    cursor.execute("insert into trivia values (?, 'spiffytech')", (fact,))
+
+dbConn.commit()
