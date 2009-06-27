12de5fec8390f22f3146730596a2a1fe9d0d8f96
Brian Cottingham <brian@linux-xyhc.site>
2009-06-27 14:58:04 -0400

    Never mind on the Yahoo Weather API- rssweather.com has an easier URL, and I don't have to do my own zipcode lookups

--- weather.py
+++ weather.py
@@ -14,54 +14,31 @@
 #along with Spiffybot.  If not, see <http://www.gnu.org/licenses/>.
 
 
-import csv
-import feedparser
 import re
+import urllib
+from xml.dom import minidom  # Yeah, I wrote this before I found out about ElementTree and BeautifulSoup
 
-def getWeather(connection=None, event=None, place=None):
-    '''Replies to a message with the current weather at a given location'''
-    place = str(place)
-    if re.match("[0-9]{5}", place) == None:
-        print place
-        print "Looking up city..."
-        zipcodes = findZip(place)  # Find the zip code from the city & state
+def getWeather(irc, event, place):
+    '''Returns the current weather for a given city and state'''
+    # TODO: Find a way to let the user input a zip code instead of city/state
+    channel = event.target()
+
+    if not re.match("[0-9]{5}", place):
+        city = place.partition(", ")[0].replace(" ", "%20")
+        state = place.partition(", ")[2]
+
+        content = urllib.urlopen("http://www.rssweather.com/wx/us/" + state + "/" + city + "/rss.php")  # Grab the rssweather.com page for this location
     else:
-        zipcodes = [place]
+        content = urllib.urlopen("http://www.rssweather.com/zipcode/" + place + "/rss.php")
 
-    for zip in zipcodes:
-        url = "http://weather.yahooapis.com/forecastrss?p=" + zip + "&u=f"
-        data = feedparser.parse(url)
-        summary = data.entries[0].summary
-        summary = re.split(r'\n',re.sub('<.+?>','',summary))
+    xmldoc = minidom.parse(content)
 
-        if not summary[0].startswith("Sorry"):
-            reply = summary[2]
-            break
+    try:
+        pubDate = xmldoc.getElementsByTagName("pubDate")[0].firstChild.data  # Try to find the weather on the page
+    except: 
+        return "I don't know that city"
+    pubDate = pubDate.split(" ")[4] + " GMT " + pubDate.split(" ")[5]
+    summary = xmldoc.getElementsByTagName("description")[1].firstChild.data
+    reply = summary + " (" + pubDate + ")"
 
-    if not "reply" in locals():
-        connection.privmsg(event.target(), "No such city in our totally awesome database of cities")
-        return
-
-
-    if connection == None:
-        print reply
-    else:
-        connection.privmsg(event.target(), reply)
-
-
-def findZip(place):
-    '''Takes a city and state, then finds that place's zip code'''
-    if len(place.split(",")) > 1:
-        city = place.partition(",")[0]
-    else:
-        city = place.split()[:-2]
-    city = city.lower()
-    print city
-    state = place[-2:].upper()
-    zipcodes = []  # There is sometimes more than one zipcode for a single city
-
-    reader = csv.DictReader(open("zipcodes.csv"), ("zip", "city", "state", "latitude", "longitude", "timezone", "dst"), delimiter=",", quotechar='"')
-    for place in reader:
-        if place["city"].lower() == city and place["state"] == state:
-            zipcodes.append(place["zip"])
-    return zipcodes
+    irc.privmsg(channel, reply)
