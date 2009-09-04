eba644f29f579f118413e42e3a9878debc482164
Brian Cottingham <brian@spiffytop.spiffynet>
2009-09-04 02:35:34 -0400

    Updated help.py to use the new event class

--- help.py
+++ help.py
@@ -22,23 +22,20 @@ commands = {
     "roulette": "Randomly tries to shoot you, a la Russian Roulette",
     "tell ": "tell PERSON MESSAGE [at TIME]: Delivers a message to a user, either when they next are seen, or at a specific time",
     "tiny": "tiny URL: Returns a tinyurl-encoded url",
-    "topics": "topicd [N]: Lists the previous n topics",
+    "topics": "topics [N]: Lists the previous n topics",
     "weather": "weather CITY, STATE: Tells you the current weather at a given location",
     "woot": "Returns the current deal on woot.com, it's price, availability, and purchase URL",
 }
 
 
-def help(connection, event, args):
-    type(args)
-    print args
-    if args == "":
+def help(event):
+    if event.args == "":
         cmds = sorted(commands.keys())
         cmds = "Commands: " + ", ".join(cmds)
-        connection.privmsg(event.target(), cmds)
+        event.reply(cmds)
         return
-
-    elif args in commands:
-        connection.privmsg(event.target(), commands[args])
+    elif event.args in commands:
+        event.reply(commands[event.args])
         return
     else:
-        connection.privmsg(event.target(), "You fail at : No such command")
+        event.reply("You fail at : No such command")
