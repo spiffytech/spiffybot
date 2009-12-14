641ec4cd44eeae6910ed98641a7b436c7e4b1b55
Brian Cottingham <brian@spiffytop.(none)>
2009-12-13 23:42:03 -0500

    Forgot to remove the cheers command form the regex list last commit

--- commands.py
+++ commands.py
@@ -41,7 +41,6 @@ cmds = [
     ("(?P<cmd>^weather)(?P<args>.*)", "weather.getWeather"),
     ("(?P<cmd>^woot)(?P<args>)", "wootoff.checkWoot"),
     ("(?P<cmd>^processes)(?P<args>.*)", "sysstats.listProcesses"),
-    ("(?P<cmd>^three cheers)(?P<args>.*)", "misc.threeCheers"),
     ("(?P<cmd>^difficulty check)(?P<args>.*)", "misc.difficultyCheck"),
     ("(?P<cmd>^roll)(?P<args>.*)", "misc.roll_dice"),
     ("(?P<cmd>^(bai|goodbye|bye|good bye|shalom))(?P<args>.*)", "misc.farewell"),
