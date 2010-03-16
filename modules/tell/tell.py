4104b939f2d82d0d5839ad623df06ef3f0e2d55f
Brian Cottingham <spiffytech@gmail.com>
2010-03-16 01:08:04 -0400

    Fixed bug that caused '^baileys' to trigger against the '^bai' regex for farewell quotes

--- modules/tell/tell.py
+++ modules/tell/tell.py
@@ -49,14 +49,11 @@ def tell(event):
 
     # Parse the time to deliver the message (if any)
     deliver = event.args.split(" in ")[-1]
-#    print "deliver = " + str(deliver)
     p = pdt.Calendar(pdc.Constants())  # Use parsedatetime module to easily handle human date formatting
     deliver = p.parse(deliver)
-#    print "deliver = " + str(deliver)
     if deliver[1] == 0:  # Tried to parse an invalid time (i.e., we can't parse "stuff")
         deliver = None
     else:
-#        print "here"
         deliver = deliver[0]
         deliver = "%d-%d-%d %d:%d:%d" % (deliver[0], deliver[1], deliver[2], deliver[3], deliver[4], deliver[5])  # Format the deliver into a string for toEpoch()
         deliver = epochTools.toEpoch(deliver, format="%Y-%m-%d %H:%M:%S")  # deliverstamp for DB storage
